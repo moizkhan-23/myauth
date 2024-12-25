a1cf15aeb276eaae9cf5a56f9a16875deddce0d9
moizkhan-23 <moiz.khan2399@gmail.com>
2024-12-25 11:18:31 -0800

    add proxy in vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 import React from 'react'
-import conf  from './conf/conf'
+import conf from './conf/conf';
 import authService from './appwrite/auth';
 import SignUp from './components/SignUp';
 function App() {
+  console.log(conf.appwriteUrl);
+  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  
+  
  
 
   
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,12 +5,12 @@ const conf={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
 
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    // appwriteDataBaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    // appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_URL_COLLECTION_ID),
-    // appwriteBucketId:String(import.meta.env.VITE_APPWRITE_URL_BUCKET_ID)
-
-
-
+    
+    
+    
 }
 
 export default conf
+// appwriteDataBaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+// appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_URL_COLLECTION_ID),
+// appwriteBucketId:String(import.meta.env.VITE_APPWRITE_URL_BUCKET_ID)
